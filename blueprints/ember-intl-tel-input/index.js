39013a0f7365f7c599ea80529e16cd5112fe4897
logeshr <logeshr@freshdesks.com>
2017-03-22 18:44:55 +0530

    Bower package path fix

--- blueprints/ember-intl-tel-input/index.js
+++ blueprints/ember-intl-tel-input/index.js
@@ -2,6 +2,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('intl-tel-input', 'git://github.com/LogeshR/intl-tel-input.git');
+    return this.addBowerPackageToProject('intl-tel-input', 'LogeshR/ember-intl-tel-input#da319b08e13e6aa0b629a2e2b50d47c1bd7dfb99');
   }
 };
