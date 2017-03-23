3f7d93beefb43db3f19f6ca5076ef1952fc3d1c6
logeshr <logeshr@freshdesks.com>
2017-03-23 12:37:19 +0530

    remove intl-tel-input in bower and add from package

--- blueprints/ember-intl-tel-input/index.js
+++ blueprints/ember-intl-tel-input/index.js
@@ -3,6 +3,6 @@ module.exports = {
 
   afterInstall: function() {
     // return this.addBowerPackageToProject('intl-tel-input', 'LogeshR/ember-intl-tel-input#da319b08e13e6aa0b629a2e2b50d47c1bd7dfb99');
-    return this.addBowerPackageToProject('intl-tel-input', 'da319b08e13e6aa0b629a2e2b50d47c1bd7dfb99');
+    return this.addBowerPackageToProject('intl-tel-input', '5987c8fca8247ddd709259899bd0e46d386dfac0');
   }
 };
