afc37666062b80a3dbd3e93e3a2fa147057c3b93
logeshr <logeshr@freshdesks.com>
2017-03-23 11:49:14 +0530

    updating to only commit id for intl-tel-input

--- blueprints/ember-intl-tel-input/index.js
+++ blueprints/ember-intl-tel-input/index.js
@@ -2,6 +2,7 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('intl-tel-input', 'LogeshR/ember-intl-tel-input#da319b08e13e6aa0b629a2e2b50d47c1bd7dfb99');
+    // return this.addBowerPackageToProject('intl-tel-input', 'LogeshR/ember-intl-tel-input#da319b08e13e6aa0b629a2e2b50d47c1bd7dfb99');
+    return this.addBowerPackageToProject('intl-tel-input', 'da319b08e13e6aa0b629a2e2b50d47c1bd7dfb99');
   }
 };
